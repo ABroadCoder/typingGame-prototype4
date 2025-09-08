316f3474d00179d0e93462aba67691ce8b917b7d
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2025-09-08 13:53:42 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+timerMinutesOnes = document.getElementById('timer-minutes-ones');
+timerMinutesTens = document.getElementById('timer-minutes-tens');
+timerSecondsOnes = document.getElementById('timer-seconds-ones');
+timerSecondsTens = document.getElementById('timer-seconds-tens');
+
+function updateTimer() {
+  const seconds = timerSeconds.textContent;
+  const minutes = timerMinutes.textContent;
+  if (seconds > 0) {
+    seconds--;
+  }
+
+  if (seconds === 0) {
+    seconds = 59;
+    minutes--;
+  }
+}
+
+const secondsUpdate = setInterval(updateTimer(), 1000);
 
