49454f661a9bfee5bc1c70c4c01ab527c89baa3b
ABroadCoder <148570855+ABroadCoder@users.noreply.github.com>
2025-09-09 13:38:40 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,20 +1,34 @@
+// HTML Element References
 timerMinutesOnes = document.getElementById('timer-minutes-ones');
-timerMinutesTens = document.getElementById('timer-minutes-tens');
 timerSecondsOnes = document.getElementById('timer-seconds-ones');
 timerSecondsTens = document.getElementById('timer-seconds-tens');
 
+// Timer Logic
 function updateTimer() {
-  const seconds = timerSeconds.textContent;
-  const minutes = timerMinutes.textContent;
-  if (seconds > 0) {
-    seconds--;
-  }
+  let secondsOnes = parseInt(timerSecondsOnes.innerHTML);
+  let secondsTens = parseInt(timerSecondsTens.innerHTML);
+  let minutesOnes = parseInt(timerMinutesOnes.innerHTML);
 
-  if (seconds === 0) {
-    seconds = 59;
-    minutes--;
+  if (secondsOnes > 0) {
+    secondsOnes--;
+  } else if (secondsOnes === 0) {
+    if (secondsTens > 0) {
+      secondsOnes = 9;
+      secondsTens--;
+    } else if (secondsTens === 0) {
+      if (minutesOnes === 0 && secondsTens === 0 && secondsOnes === 0) {
+        clearInterval(startTimer);
+      } else {
+        secondsTens = 5;
+        secondsOnes = 9;
+        minutesOnes--;
+      }
+    }
   }
+  timerSecondsOnes.innerHTML = secondsOnes;
+  timerSecondsTens.innerHTML = secondsTens;
+  timerMinutesOnes.innerHTML = minutesOnes;
 }
 
-const secondsUpdate = setInterval(updateTimer(), 1000);
-
+// Timer Start
+const startTimer = setInterval(updateTimer, 1000);
